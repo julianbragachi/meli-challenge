bbe2486b1337bdc18a76b28a7de17e29ed51f9f2
Julian Bragazzi <julian.bragazzi@despegar.com>
2019-12-11 00:02:06 -0300

    add header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import SearchBar from "../SearchBar";
+import styles from "./header.styles.scss";
+
+interface Props {}
+
+const Header: React.FunctionComponent = ({}: Props) => (
+  <header className={styles.header}>
+    <div className="container">
+      <div className="row">
+        <SearchBar className="col-md-8" />
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
