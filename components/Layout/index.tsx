ea7b4373ce5f5b202269d97d9edf190a87cfee13
Julian Bragazzi <julian.bragazzi@despegar.com>
2019-12-11 18:27:41 -0300

    implement nprogress on routechange

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,10 +2,19 @@ import React, { Fragment } from "react";
 import Head from "next/head";
 import Header from "../Header";
 import "./layout.styles.scss";
+import NProgress from "nprogress";
+import Router from "next/router";
 
 interface Props {
   children?: any;
 }
+
+Router.events.on("routeChangeStart", url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const Layout: React.FunctionComponent<Props> = ({ children }) => (
   <Fragment>
@@ -14,13 +23,19 @@ const Layout: React.FunctionComponent<Props> = ({ children }) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link
-        rel="stylesheet"
+        rel="shortcut icon"
+        href="https://http2.mlstatic.com/ui/navigation/5.3.7/mercadolibre/favicon.ico"
+      />
+      <link
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossOrigin="anonymous"
+        rel="stylesheet"
       />
       <link
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <link
+        href="https://unpkg.com/nprogress@0.2.0/nprogress.css"
         rel="stylesheet"
       />
     </Head>
